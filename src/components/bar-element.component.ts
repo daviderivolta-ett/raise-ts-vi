aa4ed0b66d9dc8b8807a23436cee1d7e939a9f4a
daviderivolta-ett <davide.rivolta@grupposcai.it>
2024-06-11 10:19:41 +0200

    Working on bar menu

--- src/components/bar-element.component.ts
+++ src/components/bar-element.component.ts
@@ -7,23 +7,59 @@ export class BarElementComponent extends HTMLElement {
         const template: HTMLTemplateElement = document.createElement('template');
         template.innerHTML =
             `
-            <a href="">
-                <slot name="bar-el-label"></slot>
-                <span class="material-symbols-outlined">
+            <a class="bar-el-link" href="">
+                <span class="material-symbols-outlined icon">
                     <slot name="bar-el-icon"></slot>
                 </span>
             </a>
 
             <style>
+                :host {
+                    width: 100%;
+                    height: 100%;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                }
+
+                .bar-el-link {
+                    cursor: pointer;
+                    text-decoration: none;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    flex-direction: column;
+                    color: var(--on-surface);
+                    background-color: var(--surface-container-high);
+                    font-size: .8rem;
+                    width: 100%;
+                    height: 100%;
+                    text-align: center;
+                }
+
+                .icon {
+                    width: 66%;
+                    height: 66%;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    border-radius: 100px;
+                }
+
+                .current {
+                    background-color: red;
+                }
+
                 .material-symbols-outlined {
                     font-family: 'Material Symbols Outlined';
-                    font-size: 1.2rem;
+                    font-size: 1.5rem;
                     font-variation-settings:
                         'FILL' 0,
                         'wght' 400,
                         'GRAD' 0,
                         'opsz' 24;
                 }
+
             </style>
             `
             ;
@@ -41,20 +77,36 @@ export class BarElementComponent extends HTMLElement {
     static observedAttributes: string[] = ['href'];
 
     public attributeChangedCallback(name: string, oldValue: string, newValue: string): void {
-        console.log(oldValue);        
         if (name === 'href') {
             if (this.linkEl) this.linkEl.setAttribute('href', newValue);
         }
     }
 
     private setup(): void {
+        this.setupLinkBehaviour();
+        this.checkCurrentPage();
+    }
+
+    private setupLinkBehaviour(): void {
         if (!this.linkEl) return;
         this.linkEl.addEventListener('click', (e: Event) => {
             e.preventDefault();
             if (!this.linkEl) return;
             const href: string | null = this.linkEl.getAttribute('href');
             if (href) window.location.hash = href;
-        })
+        });
+    }
+
+    private checkCurrentPage(): void {     
+        window.addEventListener('hashchange', (e: Event) => {
+            e.preventDefault();
+            const hash: string = window.location.hash.slice(2);          
+            if (this.linkEl) {
+                const icon: HTMLSpanElement | null = this.shadowRoot.querySelector('.icon');
+                if (!icon) return;
+                hash === this.linkEl.getAttribute('href')?.slice(1) ? icon.classList.add('current') : icon.classList.remove('current');
+            }
+        });
     }
 }
 
--- src/components/bar.component.ts
+++ src/components/bar.component.ts
@@ -17,51 +17,45 @@ export class BarComponent extends HTMLElement {
         this.shadowRoot.innerHTML =
             `
             <nav class="menu" aria-label="menu">
-                <ul class="menu-list">
-                    <li><a href="/categories">Seleziona categoria</a></li>
-                    <li><a href="/around-you">Intorno a me</a></li>
-                    <li><a href="/suggested-paths">Percorsi suggeriti</a></li>
-                    <li><a href="/custom-path">Percorso personalizzato</a></li>
-                    <li><a href="/settings">Impostazioni</a></li>
-                    <li>
-                        <app-bar-el href="/settings">
-                            <span slot="bar-el-label">Categorie</span>
-                            <span slot="bar-el-icon">stacks</span>
-                        </app-bar-el>
-                    </li>
-                </ul>
+                <app-bar-el href="/categories" aria-label="categorie">
+                    <span slot="bar-el-label">Seleziona categoria</span>
+                    <span slot="bar-el-icon">stacks</span>
+                </app-bar-el>
+                <app-bar-el href="/around-you" aria-label="intorno a te">
+                    <span slot="bar-el-label">Intorno a te</span>
+                    <span slot="bar-el-icon">explore</span>
+                </app-bar-el>
+                <app-bar-el href="/suggested-paths" aria-label="percorsi suggeriti">
+                    <span slot="bar-el-label">Percorsi suggeriti</span>
+                    <span slot="bar-el-icon">personal_places</span>
+                </app-bar-el>
+                <app-bar-el href="/custom-path" aria-label="percorso personalizzato">
+                    <span slot="bar-el-label">Percorso salvato</span>
+                    <span slot="bar-el-icon">favorite</span>
+                </app-bar-el>
+                <app-bar-el href="/settings" aria-label="impostazioni">
+                    <span slot="bar-el-label">Impostazioni</span>
+                    <span slot="bar-el-icon">settings</span>
+                </app-bar-el>
             </nav>
 
             <style>
-                .menu-list {
+                .menu {
                     display: flex;
                     justify-content: space-between;
                     align-items: center;
                     padding: 0;
+                    margin: 0;
                     list-style-type: none;
+                    height: 48px;
+                    background-color: var(--surface-container-high);
                 }
             </style>
             `
     }
 
-    private setup(): void {
-        this.setupLinkBehaviour();
-    }
+    private setup(): void { }
 
-    private setupLinkBehaviour(): void {
-        const links: NodeListOf<HTMLAnchorElement> = this.shadowRoot.querySelectorAll('a');
-        if (!links) return;
-
-        links.forEach((link: HTMLAnchorElement) => {
-            link.addEventListener('click', (e: Event) => {
-                e.preventDefault();
-                const href: string | null = link.getAttribute('href');
-                if (href) {
-                    window.location.hash = href;
-                }
-            });
-        });
-    }
 }
 
 customElements.define('app-bar', BarComponent);
