789bd5b0871f04af77a1e752573f2c87ee1762ae
daviderivolta-ett <davide.rivolta@grupposcai.it>
2024-12-03 17:34:17 +0100

    Min fix

--- src/main.ts
+++ src/main.ts
@@ -22,25 +22,29 @@ import { PathService } from './services/path.service';
 import { LayerService } from './services/layer.service';
 import { BarComponent } from './components/bar.component';
 
-// Saved data
-PathService.instance.getSavedCustomPath();
-LayerService.instance.getSavedLayers();
+main();
 
-// Routing
-const router: Router = document.querySelector('app-router') as Router;
-const categoriesRoute: Route = new Route('categories', RouteType.Default, () => '<page-categories />');
-const aroundYouRoute: Route = new Route('around-you', RouteType.Page, () => '<page-around-you />');
-const settingsRoute: Route = new Route('settings', RouteType.Page, () => '<page-settings />');
-const poiRoute: Route = new Route('poi', RouteType.Page, () => '<page-poi />');
-const customPathRoute: Route = new Route('custom-path', RouteType.Page, () => '<page-custom-path />');
-const suggestedPathsRoute: Route = new Route('suggested-paths', RouteType.Page, () => '<page-suggested-paths />');
-const suggestedPathRoute: Route = new Route('selected-suggested-path', RouteType.Page, () => '<page-selected-suggested-path />');
+function main() {
+    // Saved data
+    PathService.instance.getSavedCustomPath();
+    LayerService.instance.getSavedLayers();
 
-const routes: Route[] = [categoriesRoute, aroundYouRoute, settingsRoute, poiRoute, customPathRoute, suggestedPathsRoute, suggestedPathRoute];
-router.addRoutes(routes);
+    // Routing
+    const router: Router = document.querySelector('app-router') as Router;
+    const categoriesRoute: Route = new Route('categories', RouteType.Default, () => '<page-categories />');
+    const aroundYouRoute: Route = new Route('around-you', RouteType.Page, () => '<page-around-you />');
+    const settingsRoute: Route = new Route('settings', RouteType.Page, () => '<page-settings />');
+    const poiRoute: Route = new Route('poi', RouteType.Page, () => '<page-poi />');
+    const customPathRoute: Route = new Route('custom-path', RouteType.Page, () => '<page-custom-path />');
+    const suggestedPathsRoute: Route = new Route('suggested-paths', RouteType.Page, () => '<page-suggested-paths />');
+    const suggestedPathRoute: Route = new Route('selected-suggested-path', RouteType.Page, () => '<page-selected-suggested-path />');
 
-// Settings
-SettingService.instance.getLocalStorageSettings();
+    const routes: Route[] = [categoriesRoute, aroundYouRoute, settingsRoute, poiRoute, customPathRoute, suggestedPathsRoute, suggestedPathRoute];
+    router.addRoutes(routes);
 
-const bar: BarComponent | null = document.querySelector('app-bar');
-if (bar) bar.showSettings = SettingService.instance.settings.showSettings;
+    // Settings
+    SettingService.instance.getLocalStorageSettings();
+
+    const bar: BarComponent | null = document.querySelector('app-bar');
+    if (bar) bar.showSettings = SettingService.instance.settings.showSettings;
+}
